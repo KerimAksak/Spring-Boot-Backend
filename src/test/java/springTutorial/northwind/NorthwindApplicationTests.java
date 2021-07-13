40d13dad41f58696fdc5d9daac6aef701c202324
KerimAksak <abdulkerim.aksak@outlook.com>
2021-07-13 22:31:08 +0300

    Spring Boot Project Init

---
+++ src/test/java/springTutorial/northwind/NorthwindApplicationTests.java
@@ -0,0 +1,13 @@
+package springTutorial.northwind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
