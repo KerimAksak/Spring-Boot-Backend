4e4c2f8dc92c4e47ec98bce2f1efd316cb4d8bbb
KerimAksak <abdulkerim.aksak@outlook.com>
2021-07-14 11:17:01 +0300

    Create ProductsController

---
+++ src/main/java/springTutorial/northwind/api/controllers/ProductsController.java
@@ -0,0 +1,31 @@
+package springTutorial.northwind.api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import springTutorial.northwind.business.abstracts.ProductService;
+import springTutorial.northwind.entities.concretes.Product;
+
+@RestController
+@RequestMapping("/api/products")
+// domain/api/products
+public class ProductsController {
+	
+	
+	private ProductService productService;
+	
+	@Autowired
+	public ProductsController(ProductService productService) {
+		super();
+		this.productService = productService;
+	}
+
+	@GetMapping("/getall")
+	public List<Product> getAll(){
+		return this.productService.getAll();
+	};
+}
--- src/main/java/springTutorial/northwind/business/concretes/ProductManager.java
+++ src/main/java/springTutorial/northwind/business/concretes/ProductManager.java
@@ -3,14 +3,17 @@ package springTutorial.northwind.business.concretes;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import springTutorial.northwind.business.abstracts.ProductService;
 import springTutorial.northwind.dataAccess.abstracts.ProductDao;
 import springTutorial.northwind.entities.concretes.Product;
 
+// In addition, we specify that this class is the service layer.
+@Service
 public class ProductManager implements ProductService {
 
-	// Constructor Injection; We removed the dependency on DataAccess. (tightly)
+	// Dependency Injection & Constructor Injection; We removed the dependency on DataAccess. (tightly)
 	private ProductDao prodactDao;
 	
 	// @Autowired; Since productDao is an interface, Autowired creates a corresponding class.
