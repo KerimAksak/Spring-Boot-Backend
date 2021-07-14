8461e892607c2e3449735fcfd2d69bc401543a99
KerimAksak <abdulkerim.aksak@outlook.com>
2021-07-14 22:29:22 +0300

    Create ErrorResult.java

---
+++ src/main/java/springTutorial/northwind/core/utilities/results/ErrorResult.java
@@ -0,0 +1,13 @@
+package springTutorial.northwind.core.utilities.results;
+
+public class ErrorResult extends Result {
+	
+	public ErrorResult() {
+		super(false);
+	}
+	
+	public ErrorResult(boolean success, String message) {
+		super(false, message);
+	}
+
+}
